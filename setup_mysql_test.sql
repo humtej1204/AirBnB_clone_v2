ca574f54c625c03fda7db89bfa3ba9016f73bfd2
Humberto Tejada <humtej1204@gmail.com>
2022-04-12 19:41:28 -0500

    Line 8: Correcting user name

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,4 +5,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 -- Giving privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
