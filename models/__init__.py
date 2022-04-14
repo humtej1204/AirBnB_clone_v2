cfcff6f39cabf6128413d26b20f8b0a229858685
error-rgl <rglcnd@gmail.com>
2022-04-14 05:38:56 +0200

    [Fix]: the operation of the code of the classes is optimized

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-import os
+"""This module instantiates an object of class FileStorage"""
+from os import getenv
 
-"""Environmental Variable"""
-store = os.getenv("HBNB_TYPE_STORAGE")
 
-if store == "db":
-    """This module instantiates an object of class DBStorage"""
+if getenv('HBNB_TYPE_STORAGE') == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    """This module instantiates an object of class FileStorage"""
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
--- models/base_model.py
+++ models/base_model.py
@@ -2,34 +2,41 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-"""SQLAlchemy Modules"""
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
+
 Base = declarative_base()
 
-class BaseModel():
-    """ Class Atributes """
-    id = Column(String(60), primary_key=True, nullable=False, unique=True)
-    created_at = Column(DateTime(), nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime(), nullable=False, default=datetime.utcnow())
 
+class BaseModel:
     """A base class for all hbnb models"""
+
+    # Info! Add columns to the base class for sqlalchemy.
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
+            if 'created_at' not in kwargs:
+                self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -43,18 +50,21 @@ class BaseModel():
         storage.new(self)
         storage.save()
 
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        if dictionary[_sa_instance_state]:
-            dictionary.pop(_sa_instance_state)
-        return dictionary
-
     def delete(self):
+        """Delete the current instance from the storage"""
         from models import storage
         storage.delete(self)
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = dict(self.__dict__)
+
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
+
+        dictionary["__class__"] = str(type(self).__name__)
+
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+
+        return dictionary
