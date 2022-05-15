e866f5d067518d8ff29920625f0d9e4210c5447e
Humberto Tejada <humtej1204@gmail.com>
2022-05-15 04:32:40 -0500

    Writting code

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application
+'''
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''
+    Class hello, first test class
+    '''
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    Class hbnb, second test class
+    '''
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''
+    Class c_route, thrird test class
+    '''
+    return ("C " + str(text.replace("_", " ")))
+
+
+@app.route('/python/',defaults={'text' : "is cool"} , strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    ''''''
+    return ("Python " + str(text.replace("_", " ")))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
