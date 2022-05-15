5bbd33a8b686dc0f67568b930e444e3b84cbe543
Humberto Tejada <humtej1204@gmail.com>
2022-05-15 04:14:57 -0500

    Writting code

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application
+'''
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''
+    Class hello, first test class
+    '''
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    Class hbnb, second test class
+    '''
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''
+    Class c_route, thrird test class
+    '''
+    return ("C " + str(text.replace("_", " ")))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
