9c8c79871f866d6c5f68e3cfeafabf73e6a910c9
Humberto Tejada <humtej1204@gmail.com>
2022-05-15 03:32:51 -0500

    Creating files and writting code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
 @app.route('/')
 def hello():
     '''
@@ -16,4 +16,6 @@ def hello():
     '''
     return ('Hello HBNB!')
 
-app.run(host="0.0.0.0", port=5000)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
