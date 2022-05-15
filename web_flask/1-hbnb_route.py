bfda170a5a3fb24a86a9ab523017d63ee8a7553d
Humberto Tejada <humtej1204@gmail.com>
2022-05-15 04:04:20 -0500

    Line 21: Correcting route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -18,7 +18,7 @@ def hello():
     return ('Hello HBNB!')
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     '''
     Class hbnb, second test class
